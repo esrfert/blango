25826e9a593a641b8ecd2d228e9eecc57f14e193
Esref ERTURK <esreferturk78@gmail.com>
2022-01-18 21:19:11 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -49,12 +49,14 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        "debug_toolbar",
         "crispy_forms",
         "crispy_bootstrap5",
         "blog",
     ]
-
+    INTERNAL_IPS = ["192.168.11.179"]
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
